bc7a77c66aefaf136951843772d4130eca85ee8d
tengDeloitte <117101341+uwfgit@users.noreply.github.com>
2025-05-27 15:42:23 -0400

    v1

---
+++ go.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+rm -rf ./build
+mkdir -p ./build/client ./build/portals
+
+pnpm install
+pnpm run build:qa
+
+#copy everything from dist into build/portals folder
+cp -rf dist/* build/portals/
+
+cp -rf ./src build/client/src
+cp -f ./package.json build/client/package.json
+cp -f ./package-lock.json build/client/package-lock.json
+cp -f ./.env.qa build/client/.env
+cp -f ./postcss.config.js build/client/postcss.config.js
+cp -f ./tsconfig.json build/client/tsconfig.json
+
+#remove forward slash in index.html file
+sed -i 's|src="/assets/|src="assets/|g; s|href="/assets/|href="assets/|g' build/portals/index.html
+
---
+++ vite.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
+
+// Use environment variables for base path and API endpoint
+const ENDPOINT = 'https://workshop.cfg.deloitte.com/cfg-ai-demo';
+// const BASE_PATH =  '/assistant/';
+const isDev =  process.env.NODE_ENV  === 'dev';
+// const API_ENDPOINT = process.env.VITE_API_ENDPOINT || 'http://localhost:4200';
+export default defineConfig({
+  base: './',
+  define: {
+    'process.env': {
+      VITE_API_ENDPOINT: process.env.VITE_API_ENDPOINT || 'http://localhost:4200',
+    }
+  },
+  plugins: [
+    react(),
+  ],
+  build: {
+    minify: !isDev, // Disable minification in development mode
+    sourcemap: isDev, // Enable source maps
+},
+
+  server: {
+    port: 4200,
+    proxy: {
+      '/Monolith': {
+        target: ENDPOINT,
+        changeOrigin: true,
+        secure: false,
+        preserveHeaderKeyCase: true,
+      },
+    },    
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+});
